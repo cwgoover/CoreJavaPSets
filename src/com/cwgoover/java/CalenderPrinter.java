4655385f0e8801b381e10d9ebbdf91460f9599be
ct124511 <ct124511@alibaba-inc.com>
2017-10-05 10:45:14 +0800

    add "static import" for constant

--- src/com/cwgoover/java/CalenderPrinter.java
+++ src/com/cwgoover/java/CalenderPrinter.java
@@ -13,6 +13,13 @@ package com.cwgoover.java;
 
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
+
+// 如果需要使用大量带有冗长名字的常量，就应该使用"静态导入"
+// 静态导入还有一个应用场景：算数函数
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.DAY_OF_WEEK;
+import static java.util.Calendar.MONTH;
+
 import java.util.GregorianCalendar;
 import java.util.Locale;
 
@@ -29,9 +36,9 @@ public class CalenderPrinter {
         // 默认返回当前的日期和时间
         Calendar calendar = new GregorianCalendar();
         // 当前月份
-        int curMonth = calendar.get(Calendar.MONTH);
+        int curMonth = calendar.get(MONTH);
         // 当前日期
-        int curDay = calendar.get(Calendar.DAY_OF_MONTH);
+        int curDay = calendar.get(DAY_OF_MONTH);
 
         // calculate indents of the first day
         int indents = calculateIndentOfFirstDay();
@@ -51,15 +58,15 @@ public class CalenderPrinter {
         int firstDayOfWeek = d.getFirstDayOfWeek();
 
         // 将d设置为这个月的第一天，并得到这一天为星期几
-        d.set(Calendar.DAY_OF_MONTH, 1);
-        int weekDay = d.get(Calendar.DAY_OF_WEEK);
+        d.set(DAY_OF_MONTH, 1);
+        int weekDay = d.get(DAY_OF_WEEK);
 
         // 看看需要退几步才能到这个月星期的起始日
         int indent = 0;
         while (firstDayOfWeek != weekDay) {
             indent++;
-            d.add(Calendar.DAY_OF_MONTH, -1);
-            weekDay = d.get(Calendar.DAY_OF_WEEK);
+            d.add(DAY_OF_MONTH, -1);
+            weekDay = d.get(DAY_OF_WEEK);
         }
         return indent;
     }
@@ -77,15 +84,15 @@ public class CalenderPrinter {
     private void printDays(Calendar calendar, int curMonth, int curDay, int indents) {
         boolean firstPrint = true;
         // 从头打印
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(DAY_OF_MONTH, 1);
         int firstDayOfWeek = calendar.getFirstDayOfWeek();
 
         // 一旦calendar进入下个月，就终止打印
-        while (calendar.get(Calendar.MONTH) == curMonth) {
-            int printDay = calendar.get(Calendar.DAY_OF_MONTH);
+        while (calendar.get(MONTH) == curMonth) {
+            int printDay = calendar.get(DAY_OF_MONTH);
 
             // 每个星期的第一天换行打印
-            int printMonth = calendar.get(Calendar.DAY_OF_WEEK);
+            int printMonth = calendar.get(DAY_OF_WEEK);
             if (printMonth == firstDayOfWeek) {
                 System.out.println();
             }
@@ -105,7 +112,7 @@ public class CalenderPrinter {
                 System.out.printf("%6s", printDay);
             }
             // 将calendar设置为下一天
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
+            calendar.add(DAY_OF_MONTH, 1);
         }
     }
 
